7af3b5296207aec3a06e6bba21d48953405d6686
Bonginkosi-BBT97 <brightwelltshabalala@gmail.com>
2022-08-23 07:36:34 +0200

    Initial Commit

---
+++ To-Do-List-UsingCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do-List-UsingCoreData
+//
+//  Created by Bonginkosi Tshabalala on 2022/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
